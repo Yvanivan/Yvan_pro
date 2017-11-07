a26e31391a3c39f5eed99350023f3d7704eac509
Yvanivan <yvanivan@163.com>
2017-11-07 19:25:17 +0800

    add

---
+++ dnn/__init__.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 _*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+""" 
+@author:Yesus 
+@file: __init__.py.py 
+@time: 2017/11/07 
+""" 
---
+++ dnn/demo.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 _*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+""" 
+@author:Yesus 
+@file: demo.py 
+@time: 2017/11/07 
+""" 
